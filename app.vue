44e5356ad6ad3bcd248497c893fef3880ba051d3
zcf0508 <zcf0508@live.com>
2024-08-30 10:58:34 +0800

    init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <WithRefAndWithSlot ref="aaaRef"></WithRefAndWithSlot>
+  </div>
+</template>
+<script setup lang="ts">
+
+const aaaRef = ref<ComponentInstance['WithRefAndWithSlot']>()
+</script>
---
+++ components/WithRefAndWithSlot.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ATree ref="treeRef"></ATree>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+const treeRef = ref<ComponentInstance['ATree']>()
+
+</script>
+
+<style scoped>
+
+</style>
